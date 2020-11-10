dbbe552dd386f0b6d88b254c7fc79b731a8db735
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-11-11 00:22:49 +0300

    Feature silmek için php sayfası oluşturuldu

---
+++ delete_feature.php
@@ -0,0 +1,46 @@
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$msg = '';
+
+//check if the given item with the primary key exists
+if (isset($_GET['FEATURE_ID'])) {
+    $stmt = $pdo->prepare('SELECT * FROM FEATURES WHERE FEATURE_ID = ?');
+    $stmt->execute([$_GET['FEATURE_ID']]);
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$product) {
+        exit('Feature doesn\'t exist with that primary key!');
+    }
+
+    //Asking the users second time if they really want to delete the item
+    if (isset($_GET['confirm'])) {
+        if ($_GET['confirm'] == 'yes') {
+            $stmt = $pdo->prepare('DELETE FROM FEATURES WHERE FEATURE_ID = ?');
+            $stmt->execute([$_GET['FEATURE_ID']]);
+            $msg = 'You have deleted the selecteed product!';
+        } else {
+            header('Location: read_feature.php');
+            exit;
+        }
+    }
+} else {
+    exit('No primary key has been specified!');
+}
+?>
+
+<?=template_header('Delete')?>
+
+<div class="feature delete">
+	<h2>Delete Feature #<?=$product['FEATURE_ID']?></h2>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php else: ?>
+	<p>Are you sure you want to delete the selected feature #<?=$product['FEATURE_ID']?>?</p>
+    <div class="yesno">
+        <a href="delete_feature.php?FEATURE_ID=<?=$product['FEATURE_ID']?>&confirm=yes">Yes</a>
+        <a href="delete_feature.php?FEATURE_ID=<?=$product['FEATURE_ID']?>&confirm=no">No</a>
+    </div>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>

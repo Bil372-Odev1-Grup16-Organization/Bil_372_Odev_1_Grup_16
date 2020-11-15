8c7f9e5f3c28ec2330e61857e8d9450b71bbec00
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-11-15 18:47:50 +0300

    Brand ve Organizasyonu birleştirme sayfası güncellendi

--- create_brand_organisations.php
+++ create_brand_organisations.php
@@ -1,26 +1,79 @@
 <?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
 
-$pdo = new PDO('mysql:host=localhost;dbname=TEST', 'root', 'admin123');
-$sql = "SELECT ORGANISATION.ORG_ID, PRODUCT_BRANDS.BRAND_BARCODE FROM ORGANISATION, PRODUCT_BRANDS";
+if (isset($_POST['submit'])) {
+    echo "Don't leave any field empty";
+    $id = isset($_POST['LOT_ID']) && !empty($_POST['LOT_ID']) && $_POST['LOT_ID'] != 'auto' ? $_POST['LOT_ID'] : NULL;
+    $stmt = $pdo->prepare('INSERT INTO BRAND_ORGS VALUES (?,?,?,?,?,?,?,?,?)');
+    if($stmt->execute([$id,$_POST['ORG_ID'], $_POST['BRAND_BARCODE'],$_POST['UNIT'], $_POST['EXPIRY_DATE'],$_POST['BASE_PRICE'], $_POST['IN_AMOUNT'],$_POST['OUT_AMOUNT'],$_POST['QUANTITY']])){
+        echo("<script>alert('Created Successfully')</script>");
+        echo("<script>window.location = 'read_product_features.php';</script>");
+    }
+}
+
+$sql = "SELECT ORG_ID, ORG_NAME FROM ORGANISATIONS";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
-$users = $stmt->fetchAll();
+$products = $stmt->fetchAll();
+
+$sql = "SELECT BRAND_BARCODE, BRAND_NAME FROM PRODUCT_BRANDS";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$features = $stmt->fetchAll();
+
+
+
 ?>
+<style>
+select {
+  width: 400px;
+  height:43px;
+  border-radius: 4px;
+
+}
+</style>
 
 
-<form action="submit_brand_organisations.php" method="post">
-    <select name="organisations">
-        <?php foreach($users as $user): ?>
-            <option value="<?= $user['ORG_ID']; ?>"><?= $user['ORG_ID']; ?></option>
 
+
+
+<?=template_header('Create')?>
+
+<div class="content update">
+<form action="" method="post">
+    <label for="Organisation">Organisation</label>
+    <label for="Brand">Brand</label>
+    <select name="Organisation" required="required">
+        <option disabled selected>Select an Organisation </option>
+        <?php foreach($products as $product): ?>
+            <option value="<?= $product['ORG_ID']; ?>"><?= $product['ORG_NAME']; ?></option>
         <?php endforeach; ?>
     </select>
 
-    <select name="product_brands">
-        <?php foreach($users as $user): ?>
-            <option value="<?= $user['BRAND_BARCODE']; ?>"><?= $user['BRAND_BARCODE']; ?></option>
-
+    <select name="Brand" style="margin-left: 25px">
+        <option disabled selected required="required">Select a brand </option>
+        <?php foreach($features as $feature): ?>
+            <option value="<?= $feature['BRAND_BARCODE']; ?>"><?= $feature['BRAND_NAME']; ?></option>
         <?php endforeach; ?>
     </select>
-    <input type="submit" name="submit">
+
+    <label for="UNIT">UNIT </label>
+    <label for="EXPIRY_DATE">EXPIRY_DATE</label>
+    <input type="number" name="UNIT" placeholder="example value" min="0" required="required">
+    <input type="date" name="EXPIRY_DATE" placeholder="example value" required="required">
+
+    <label for="BASE_PRICE">BASE_PRICE </label>
+    <label for="IN_AMOUNT">IN_AMOUNT</label>
+    <input type="number" name="BASE_PRICE" placeholder="example value" min="0" required="required">
+    <input type="number" name="maxval" placeholder="example value" min="0" required="required">
+
+    <label for="OUT_AMOUNT">OUT_AMOUNT </label>
+    <label for="QUANTITY">QUANTITY</label>
+    <input type="number" name="minval" placeholder="example value" min="0" required="required">
+    <input type="number" name="maxval" placeholder="example value" min="0" required="required">
+
+    <input type="submit" name="submit" value="Link">
 </form>
+</div>
+

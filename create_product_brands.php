13e8d8277c2439142a702cbfc0da178c141087fd
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-11-16 09:57:18 +0300

    bug fix & Manufacturer ve Product için dropdown

--- create_product_brands.php
+++ create_product_brands.php
@@ -7,12 +7,34 @@ if (!empty($_POST)) {
     $barcode = isset($_POST['BRAND_BARCODE']) ? $_POST['BRAND_BARCODE'] : '';
     $name = isset($_POST['BRAND_NAME']) ? $_POST['BRAND_NAME'] : '';
     $id = isset($_POST['MANUFACTURER_ID']) ? $_POST['MANUFACTURER_ID'] : '';
+    $m_syscode = isset($_POST['M_SYSCODE']) ? $_POST['M_SYSCODE'] : '';
 
     $stmt = $pdo->prepare('INSERT INTO PRODUCT_BRANDS VALUES (?, ?, ?, ?)');
-    $stmt->execute([$barcode, $name, $id, $code]);
+    $stmt->execute([$barcode, $name, $id, $m_syscode]);
     $msg = 'Created Successfully!';
 }
+
+$sql = "SELECT M_SYSCODE, M_NAME FROM PRODUCT";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$product = $stmt->fetchAll();
+
+
+$sql = "SELECT MANUFACTURER_ID, MANUFACTURER_NAME FROM MANUFACTURERS";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$manufacturer = $stmt->fetchAll();
+
 ?>
+
+<style>
+select {
+  width: 400px;
+  height:43px;
+  border-radius: 4px;
+
+}
+</style>
 
 <?=template_header('Create')?>
 
@@ -24,11 +46,23 @@ if (!empty($_POST)) {
       <input type="text" name="BRAND_BARCODE" placeholder="example value" id="BRAND_BARCODE">
       <input type="text" name="BRAND_NAME" placeholder="example value" id="BRAND_NAME">
 
-      <!-- MANUFACTURER_NAME will be selected by a dropdown menu <-->
+
+      <label for="M_SYSCODE">Product</label>
       <label for="MANUFACTURER_NAME">MANUFACTURER_NAME</label>
-      <label></label>
-      <input type="text" name="MANUFACTURER_ID" placeholder="example value" id="MANUFACTURER_ID">
-      <label></label>
+
+      <select name="M_SYSCODE" required="required">
+          <option disabled selected>Select a manufacturer </option>
+          <?php foreach($product as $product): ?>
+              <option value="<?= $product['M_SYSCODE']; ?>"><?= $product['M_NAME']; ?></option>
+          <?php endforeach; ?>
+      </select>
+
+      <select name="MANUFACTURER_ID" required="required">
+          <option disabled selected>Select a manufacturer </option>
+          <?php foreach($manufacturer as $manufacturer): ?>
+              <option value="<?= $manufacturer['MANUFACTURER_ID']; ?>"><?= $manufacturer['MANUFACTURER_NAME']; ?></option>
+          <?php endforeach; ?>
+      </select>
 
       <input type="submit" value="Create">
     </form>
@@ -38,3 +72,4 @@ if (!empty($_POST)) {
 </div>
 
 <?=template_footer()?>
+

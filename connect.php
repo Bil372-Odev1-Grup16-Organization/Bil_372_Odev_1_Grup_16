09131e35799258b4d3c6e91dce2b3f35a6fa1341
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-11-17 22:38:40 +0300

    Şifre ve database name değiştirildi

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$database = "Odev1";
+$password = "rootroot";
+$database = "odev1";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 

844176d08b5dab91521b3cd15112372fe6be77b6
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-11-17 22:38:20 +0300

    Şifre değiştirildi

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function pdo_connect_mysql() {
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
+    $DATABASE_PASS = 'rootroot';
     $DATABASE_NAME = 'odev1';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
